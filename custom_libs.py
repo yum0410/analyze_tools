11d9d6f27717d0db7eb5dfe08f32431a9ab3a2fe
FT-yuki <yakinikug1010@gmail.com>
2020-05-12 20:24:04 +0900

    Create custom_lib.py
    add func counter_fill_key

---
+++ custom_libs.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+
+def counter_fill_key(list):
+    cnt = Counter(list)
+    max_iter = max(cnt.keys())
+    for i in range(0, max_iter+1):
+        if cnt.get(i) is None:
+            cnt[i] = 0
+    return {k: v for k, v in sorted(cnt.items(), key=lambda x:x[0])}
+
+if __name__ == "__main__":
+    hoge = [1,1,3,4,5,6,9,9,9,10]
+    print(counter_fill_key(hoge))
