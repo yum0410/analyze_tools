adf3c49b4382a3fbf743594dbf61d71500980605
FT-yuki <yakinikug1010@gmail.com>
2020-06-10 22:49:08 +0900

    Add print_len_set

--- custom_libs.py
+++ custom_libs.py
@@ -9,10 +9,14 @@ def counter_fill_key(list):
             cnt[i] = 0
     return {k: v for k, v in sorted(cnt.items(), key=lambda x:x[0])}
 
+def print_len_set(x: list):
+    print(len(set(x)))
+
 def set_sort(x: list):
     return set(sorted(x))
 
 if __name__ == "__main__":
     hoge = [1,1,3,4,5,6,9,9,9,10]
     print(counter_fill_key(hoge))
+    print_len_set(hoge)
     print(set_sort(hoge))
