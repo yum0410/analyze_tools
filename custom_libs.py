efbc63cfe80516324e531235c61a8656358b3986
FT-yuki <yakinikug1010@gmail.com>
2020-06-10 23:05:55 +0900

    Add sub_list

--- custom_libs.py
+++ custom_libs.py
@@ -15,8 +15,13 @@ def print_len_set(x: list):
 def set_sort(x: list):
     return set(sorted(x))
 
+def sub_list(list_x: list, list_y: list):
+    return  [x for x in list_x if x not in list_y]
+
 if __name__ == "__main__":
     hoge = [1,1,3,4,5,6,9,9,9,10]
     print(counter_fill_key(hoge))
     print_len_set(hoge)
     print(set_sort(hoge))
+    huga = [1, 2, 3]
+    print(sub_list(hoge, huga))
